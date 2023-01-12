7dcd91e4a8fcb12507c447f926fd3c2436af8fcd
ixtk <ikhcamot@gmail.com>
2023-01-12 20:01:16 +0400

    Work on responsive roadmap page

---
+++ src/components/FeedbackStack.tsx
@@ -0,0 +1,31 @@
+import { FeedbackCard } from "components/FeedbackCard"
+import React from "react"
+
+export const FeedbackStack = ({
+  feedbackStatus
+}: {
+  feedbackStatus: string
+}) => {
+  return (
+    <div className="p-3">
+      <h2 className="mt-1 mb-4 flex flex-col text-lg font-semibold text-secondary-800">
+        <span className="hidden lg:inline-block">{feedbackStatus}</span>
+        <span className="hidden text-sm font-normal text-secondary-700 lg:inline-block">
+          Ideas prioritized for research
+        </span>
+        <span className="lg:hidden">Ideas prioritized for research</span>
+      </h2>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-[repeat(auto-fill,minmax(40%,auto))] lg:grid-cols-1">
+        <div className="rounded-xl border-t-4 border-t-accentSecondary">
+          <FeedbackCard />
+        </div>
+        <div className="rounded-xl border-t-4 border-t-accentSecondary">
+          <FeedbackCard />
+        </div>
+        <div className="rounded-xl border-t-4 border-t-accentSecondary">
+          <FeedbackCard />
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/roadmap.tsx
+++ src/pages/roadmap.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link"
-import { Button } from "../components/Button"
-import { FeedbackCard } from "../components/FeedbackCard"
-import ArrowIcon from "../assets/icons/arrow.svg";
-import React from "react";
+import { Button } from "components/Button"
+import ArrowIcon from "assets/icons/arrow.svg"
+import React from "react"
+import { FeedbackStack } from "components/FeedbackStack"
 
 const Roadmap = () => {
   return (
-    <div>
-      <div className="bg-secondary-900 px-3 py-5 font-bold text-base-100">
-        <div className="flex justify-between items-center">
+    <div className="md:mx-auto md:max-w-7xl md:py-10 md:px-3">
+      <div className="bg-secondary-900 px-3 py-5 font-bold text-base-100 md:rounded-xl">
+        <div className="flex items-center justify-between">
           <div>
-            <Link href="/" className="flex items-center gap-x-3 mb-3">
+            <Link href="/" className="mb-3 flex items-center gap-x-3">
               <ArrowIcon className="rotate-90" />
               <span className="text-sm">Go back</span>
             </Link>
@@ -19,30 +19,30 @@ const Roadmap = () => {
           <Button text="+ Add feedback" variant="accent" />
         </div>
       </div>
-      <ul className="flex border-b border-b-base-400 text-center">
+      <ul className="flex border-b border-b-base-400 text-center lg:hidden">
         <li className="flex-1 border-b-4 border-b-accentPrimary">
-          <button className="py-4 w-full text-sm text-secondary-900 font-bold">Planned (12)</button>
+          <button className="w-full py-4 text-sm font-bold text-secondary-900">
+            Planned (12)
+          </button>
         </li>
         <li className="flex-1 border-b-4 border-opacity-0">
-          <button className="py-4 w-full text-sm text-secondary-900 font-bold">In progress (3)</button>
+          <button className="w-full py-4 text-sm font-bold text-secondary-900">
+            In progress (3)
+          </button>
         </li>
         <li className="flex-1 border-b-4 border-opacity-0">
-          <button className="py-4 w-full text-sm text-secondary-900 font-bold">Live (4)</button>
+          <button className="w-full py-4 text-sm font-bold text-secondary-900">
+            Live (4)
+          </button>
         </li>
       </ul>
-      <div className="flex flex-col gap-y-4 p-3">
-        <h2 className="my-3 text-lg font-semibold text-secondary-800">
-          Ideas prioritized for research
-        </h2>
-        <div className="rounded-xl border-t-4 border-t-accentSecondary">
-          <FeedbackCard />
-        </div>
-        <div className="rounded-xl border-t-4 border-t-accentSecondary">
-          <FeedbackCard />
-        </div>
-        <div className="rounded-xl border-t-4 border-t-accentSecondary">
-          <FeedbackCard />
-        </div>
+      <div className="lg:hidden">
+        <FeedbackStack feedbackStatus="Planned" />
+      </div>
+      <div className="hidden grid-flow-col grid-cols-[40%_40%_40%] overflow-x-scroll pb-5 lg:grid xl:grid-cols-3 xl:overflow-x-visible">
+        <FeedbackStack feedbackStatus="Planned" />
+        <FeedbackStack feedbackStatus="In-progress" />
+        <FeedbackStack feedbackStatus="Live" />
       </div>
     </div>
   )
