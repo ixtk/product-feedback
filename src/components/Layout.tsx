76dbb590d58674c9e3cd4c7a6caf699f656a2e16
ixtk <ikhcamot@gmail.com>
2023-02-01 15:57:41 +0400

    Fix document title rendering issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,10 +13,12 @@ const jost = Jost({
 })
 
 export const Layout = (props: LayoutProps) => {
+  const pageTitle = `${props.pageTitle} | Feedback Board`
+
   return (
     <>
       <Head>
-        <title>{props.pageTitle} | Feedback Board</title>
+        <title>{pageTitle}</title>
       </Head>
       <main className={clsx(jost.variable, "min-h-full bg-base-300 font-sans")}>
         {props.children}
