a3281dd3fcc37410f1626d163db92eb9c0d64ab6
ixtk <ikhcamot@gmail.com>
2023-02-16 14:04:50 +0400

    Add user images in comment section

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
       use: ["@svgr/webpack"]
     })
     return config
+  },
+  images: {
+    domains: ["res.cloudinary.com"]
   }
 }
 
--- src/components/comment/CommentHeader.tsx
+++ src/components/comment/CommentHeader.tsx
@@ -1,4 +1,5 @@
 import { User } from "shared/types"
+import Image from "next/image"
 
 interface CommentHeaderProps {
   user: User
@@ -9,8 +10,13 @@ interface CommentHeaderProps {
 export const CommentHeader = (props: CommentHeaderProps) => {
   return (
     <header className="flex items-center justify-between gap-x-2 md:gap-x-4">
-      {/* temporary placeholder for image */}
-      <div className="col-start-1 h-10 w-10 rounded-full bg-secondary-700"></div>
+      <Image
+        src={`https://res.cloudinary.com/dc2c49xov/image/upload/users/${props.user.image}`}
+        alt={props.user.name}
+        width={80}
+        height={80}
+        className="col-start-1 h-10 w-10 rounded-full bg-base-400"
+      />
       <address className="flex-1 not-italic">
         <h3 className="font-medium">{props.user.name}</h3>
         <span className="text-secondary-700">@{props.user.username}</span>
