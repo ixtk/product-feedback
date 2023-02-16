b1c2aa9c66dd370bcf9b2add15cfa1978ad9211f
ixtk <ikhcamot@gmail.com>
2023-02-16 13:13:53 +0400

    Add feedback upvote functionality

--- src/components/feedback/UpvoteButton.tsx
+++ src/components/feedback/UpvoteButton.tsx
@@ -1,5 +1,6 @@
 import UpvoteArrowIcon from "assets/icons/arrow.svg"
 import clsx from "clsx"
+import { useEffect, useState } from "react"
 
 interface UpvoteButtonProps {
   upvoteCount: number
@@ -10,15 +11,38 @@ export const UpvoteButton = ({
   onRoadmapCard = false,
   ...props
 }: UpvoteButtonProps) => {
+  const [upvoteCount, setUpvoteCount] = useState(props.upvoteCount)
+  const [selected, setSelected] = useState(false)
+
+  useEffect(() => {
+    setUpvoteCount(props.upvoteCount)
+  }, [props.upvoteCount])
+
+  const toggleUpvote = () => {
+    if (selected) {
+      setUpvoteCount(prevCount => prevCount - 1)
+    } else {
+      setUpvoteCount(prevCount => prevCount + 1)
+    }
+    setSelected(prevUpvoted => !prevUpvoted)
+  }
+
   return (
     <button
+      aria-pressed={selected}
+      onClick={toggleUpvote}
       className={clsx(
-        "rounded-corners col-start-1 row-start-2 flex items-center gap-2 justify-self-start border border-primary-200 bg-base-300 py-2 px-4 text-sm font-medium text-secondary-900 transition-colors duration-100 hover:bg-primary-200",
-        !onRoadmapCard && "md:w-11 md:flex-col md:self-start md:px-2"
+        "rounded-corners col-start-1 row-start-2 flex items-center gap-2 justify-self-start border border-primary-200 py-2 px-4 text-sm font-medium transition-colors duration-100",
+        !onRoadmapCard && "md:w-11 md:flex-col md:self-start md:px-2",
+        selected
+          ? "bg-primary-600 text-base-100 hover:bg-primary-500"
+          : "bg-base-300 text-secondary-900 hover:bg-primary-200"
       )}
     >
-      <UpvoteArrowIcon className="rotate-180 text-primary-600" />
-      <span className="font-bold text-secondary-800">{props.upvoteCount}</span>
+      <UpvoteArrowIcon
+        className={clsx("rotate-180", !selected && "text-primary-600")}
+      />
+      <span className="font-bold">{upvoteCount}</span>
     </button>
   )
 }
